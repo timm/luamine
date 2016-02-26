0b056a112d219a320471d74d537237c48ab9ff05
Tim Menzies <tim.menzies@gmail.com>
2016-02-26 16:32:41 -0500

    saving

--- lua2/lines.lua
+++ lua2/lines.lua
@@ -3,30 +3,41 @@ require "magic"
 
 --[[
 
-Kills white space and comments.
-Skips blank lines.
-Lines ending with the seperator are continued to the next.
-Lines are split on the seperator.
+INPUT:  Text files contain comma seperated values.
+OUTPUT: One table per row of the form {x=t1,y=t2} where t1 
+        are the independent  variables and t2 are the 
+        dependent variables (and all num strings coerced
+        to numbers)
+SYNOPSIS:
 
-First line is a list of names for the columns.
-Other lines are data.
-Names have magic symbols as defined in magic.lua
-Columns with names with the ignore character are ignored.
+    io.input("data.csv") 
+    for n,row in lines() do
+      -- handle row n
+    end
 
-Columns with names with the numeric character are treated special.
-Specifically, cells in numerc columns are coerced to numbers
+Note this code runs as an iterator that reads one
+line at a time into RAM (lets learners decide how
+much information, if any, they want to keep around).
+
+FEATURES:
+
+- Kills white space and comments.
+- Skips blank lines.
+- Lines ending with the seperator are continued to the next.
+- Lines are split on the seperator.
+- First line is a list of names for the columns.
+- Other lines are data.
+- Names have magic symbols as defined in magic.lua
+- Columns with names with the ignore character are ignored.
+- Columns with names with the numeric character are treated special.
+- Specifically, cells in numerc columns are coerced to numbers
   (unless they are just the missing character)
 
-Rows are returned as {x=t1,y=t2} where t1 are the independent 
-  variables and t2 are the dependent variables.
+PERFORMACE:
 
-All this is in an iterator that reads one line at
-a time into RAM (lets learners decide how much information, 
-if any,  they want to keep around).
-
-FYI, this code eats 10MB of a CSV file 
-with 18 rows in 2.2 secs, using luajit. And that is
-after near zero work on optimizations.
+This code eats 10MB of a CSV file with 18 rows in
+2.2 secs, using luajit. And that is after near zero
+work on optimizations.
 
 --]]
 
