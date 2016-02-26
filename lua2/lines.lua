5a623c7e802fc3ff413765a24e25abd7246a36b7
Tim Menzies <tim.menzies@gmail.com>
2016-02-26 13:50:23 -0500

    saving

--- lua2/lines.lua
+++ lua2/lines.lua
@@ -32,22 +32,22 @@ after near zero work on optimizations.
 
 do
   local function asNumber(x)
-    return x == Chars.missing and x or tonumber(x)
+    return x==Chars.missing and x or tonumber(x)
   end
-
+  
   local function xyMaker(cells)
      local nx,ny,maker = 0,0,{}
      for col,cell in ipairs(cells) do
       if not found(cell,Chars.ignorep) then
-	local prep = found(cell,Chars.nump) and asNumber
+        local prep = found(cell,Chars.nump) and asNumber
 	             or same
-	if found(cell, Chars.dep)
-	then ny = ny + 1
-	     add(maker, {col=col, xy= 2,
-	  	         at=ny,prep=prep})
-	else nx = nx + 1
-	     add(maker, {col=col,xy=1,
-		         at=nx,prep=prep})
+        if   found(cell, Chars.dep)
+        then ny = ny + 1
+             add(maker, {col=col, xy= 2,
+                         at=ny,prep=prep})
+        else nx = nx + 1
+             add(maker, {col=col,xy=1,
+                        at=nx,prep=prep})
      end end end
      return maker
   end
@@ -64,30 +64,26 @@ do
   end
   
   function lines()
-    local n, header, makexy = 0, {},  nil
+    local n, header, make = 0, {},  nil
     local function newLine(str)
       n = n + 1
       local tmp = explode(str,Chars.sep)
-      if n == 1 then
-	makexy=xyMaker(tmp)
-      end
-      return n, t2xy(tmp,makexy,n)
+      if n == 1 then make=xyMaker(tmp) end
+      return n, t2xy(tmp,make,n)
     end
     return function()
       local pre, line = "", io.read()
       while line ~= nil do
-	line = line:gsub(Chars.white,""):gsub(Chars.comment,"")
-	if line ~= "" then
-	  if lastchar(line) == "," then
-	    pre  = pre .. line
-	  else
-	    line =  pre .. line
-	    pre  = ""
-	    return newLine(line)
-	end end
-	line = io.read()
+        line = line:gsub(Chars.white,""):gsub(Chars.comment,"")
+        if line ~= "" then
+          if   lastchar(line) == ","
+          then pre  = pre .. line
+          else line =  pre .. line
+               pre  = ""
+               return newLine(line)
+         end end
+       line = io.read()
       end
-      if len(pre) > 0 then
-	return newLine(pre)
-  end end end
+      if len(pre) > 0 then return newLine(pre) end
+  end end 
 end
