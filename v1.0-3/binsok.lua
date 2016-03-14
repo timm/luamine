d9a2df7094095d98bbd780ba3c17771bded68624
Tim Menzies <tim.menzies@gmail.com>
2016-03-14 00:06:21 -0400

    saving

---
+++ v1.0-3/binsok.lua
@@ -0,0 +1,35 @@
+require "xy"
+require "bins"
+
+if arg[1] == "--binWeather" then
+  local opts = split0()
+  opts.verbose = true
+  local nums = map(xy().rows,
+	            function (row) return row.x[2] end)
+  local b = bins(nums, opts)
+  assert(b[2].lo == 69)
+  assert(b[2].up == 72)
+end
+
+if arg[1] == "--binMaxwell" then
+  local opts = split0()
+  opts.verbose = true
+  local nums =  map(xy().rows,
+			function (row) return row.x[24] end)
+  local b = bins(nums, opts)
+  for _,b1 in ipairs(b) do
+    print(b1)
+  end
+end
+
+if arg[1] == "--binMaxwell100" then
+  rseed(1)
+  local opts = split0()
+  opts.verbose = false
+  local nums = map(xy().rows,
+			function (row) return row.x[24] end)
+  local b = bins(nums, opts)
+  for _,b1 in ipairs(b) do
+    print(b1)
+  end
+end
