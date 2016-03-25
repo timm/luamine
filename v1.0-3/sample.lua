1cc1a7e7a034d88fbd802bb7cfe6880d302689fe
Tim Menzies <tim.menzies@gmail.com>
2016-03-25 13:53:05 -0400

    saving

--- v1.0-3/sample.lua
+++ v1.0-3/sample.lua
@@ -2,37 +2,41 @@ require "xy"
 
 do
   local id=0
-  function sample0(names)
+  function sample0(klasses)
     id = id + 1
-    return { id = id,
-	     rows = {},
-	     names= names or {},
-	     metas = {x={}, y={}} }
+    return { id      = id,
+	     rows    = {},
+	     klasses = kclasses,
+	     columns = {x={}, y={}} }
   end
   -------------------------------  
-  local function row0(data, metas,    meta.put)
-    for col,x in ipairs(data) do
-      meta      = type(x)=='number' and num0() or sym0()
-      meta.put  = type(x)=='number' and num1   or sym1
-      metas[col]= meta
+  local function row0(data, columns, names, k,   col)
+    for j,x in ipairs(data) do
+      col     = type(x)=='number' and num0() or sym0()
+      col.put = type(x)=='number' and num1   or sym1
+      if names then
+	col.txt = names[j+k]
+      end
+      columns[j] = col
   end end
   -------------------------------  
-  local function row1(data, metas)
-    for col,x in ipairs(data) do
+  local function row1(data, columns,      col,put)
+    for j,x in ipairs(data) do
       if x ~="_" and x ~= nil then
-	puts = metas[col].puts
-	puts( x, metas[col] )
+	col = columns[i]
+	put = col.put
+	put(x, col)
   end end end
   -------------------------------  
-  function sample1(row,t,names)
-    t = t or sample0(names)
-    if #t.rows==0 then
-      row0(row.x, t.metas.x)
-      row0(row.y, t.metas.y)
+  function sample1(row, t,   names, klasses)
+    t = t or sample0(klasses)
+    if #t.rows == 0 then
+      row0(row.x, t.columns.x, names, 0)
+      row0(row.y, t.columns.y, names, #row.x)
     end
-    row1(row.x, t.metas.x)
-    row1(row.y, t.metas.y)
-    t.rows[#t.rows+1] = row
+    row1(row.x, t.columns.x)
+    row1(row.y, t.columns.y)
+    t.rows[#t.rows + 1] = row
     return t
   end
 end
