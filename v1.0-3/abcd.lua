ccf12c5215b16ed2ba8649b071e4527c1bfec4c3
Tim Menzies <tim.menzies@gmail.com>
2016-03-28 23:17:38 -0400

    saving

--- v1.0-3/abcd.lua
+++ v1.0-3/abcd.lua
@@ -20,13 +20,39 @@ do
   function abcd1(actual, predicted, t)
     known(actual,    t)
     known(predicted, t)
-    if actual == predicted then
-      t.yes = t.yes + 1
-    else
-      t.no  = t.no + 1
+    if   actual == predicted then
+         t.yes = t.yes + 1
+    else t.no  = t.no  + 1 end 
+    for x,_ in pairs(t.known) do
+      if actual == x then
+	if   actual == predicted then
+	     t.d[x] = t.d[x] + 1
+	else t.b[x] = t.b[x] + 1 end 
+      else
+	if   predict == x then
+	     t.c[x] = t.c[x] + 1
+	else t.a[x] = t.a[x] + 1 end 
+      end
+  end end
+  local function p(x) return floor(100*y + 0.5) end
+  local function n(x) return floor(x)           end
+  ------------------------------------------------
+  function abcdz(t)
+    local pd,pf,pn,prec,g,f,acc = 0,0,0,0,0,0,0
+    local out = {}
+    for x,_ in pairs(t.known) do
+      local a,b,c,d = t.a[x], t.b[x], t.c[x], t.d[x]
+      if b+d > 0          then pd = d / (b+d) end
+      if a+c > 0          then pf = c / (a+c) end
+      if a+c > 0          then pn = (b+d) / (a+c) end
+      if 1-pf+pd > 0      then g = 2 * (1-pf) * pd / (1-pf+pd) end
+      if prec+pd > 0      then f = 2*prepc*pd / (prec + d)     end
+      if t.yes + t.no > 0 then acc = t.yes / (t.yes + t.no) end
+      out[x] = {db=t.db, rx = t.rx, yes = n(b+d), all=n(a+b+c+d),
+		a=n(a),  b=n(b),    c=n(c), d= n(d), acc= p(acc),
+		p=p(pd), pf=p(pf),  prec=p(prec),
+		f=p(f),  g=p(g),    x=x}
     end
-    for k,v in pairs(i.known) do
-      if actual = 
-    end
+    return out
   end
 end
