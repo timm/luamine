ec2c61aa2a18ab97c072b14dfaddec85eb6a7918
Tim Menzies <tim.menzies@gmail.com>
2016-04-01 00:10:23 -0400

    saving

--- v1.0-3/nb.lua
+++ v1.0-3/nb.lua
@@ -1,49 +1,44 @@
 require "sample"
 
 do
+  -----------------------------------------------------
   function nb0() return {
       m = 2,
       k = 1,
       ignore = "_",
       enough = 10}
   end
-  local function nbtest(row,n,t)
-    for k,v in pairs(row) do
-      if v ~=  n.ignore then
-      end
-    end
-  end
-  local function predict(row,n,t,sizes,nh,most,out)
+  -----------------------------------------------------    
+  local function predict(row,opt,t,sizes,nh,most,out)
     for h,columns in pairs(t.subs) do
       local size  = columns.y[1].n
-      local prior = (size + n.k) / (n + k*nh)
-      local tmp   = prior
-      for j,x in pairs(row) do
-	if x ~= n.ignore then
-	  local col = columns[j]
-	  local inc
+      local prior = (size + opt.k) / (n + k*nh)
+      local tmp   = math.log(prior)
+      for j,col in pairs(columns.x) do
+	local x = row.x[j]
+	if x ~= opt.ignore then
+	  local inc = 0
 	  if col.put == num1 then
-	    inc =  normpdf(x,col)
+	    inc = normpdf(x,col)
 	  else
-	    local f = (col[x] or 0) + (n.m*prior)
-	    inc = f/(size + n.m)
+	    local f = (col.counts[x] or 0) + (opt.m * prior)
+	    inc = f/(size + opt.m)
 	  end
-	  tmp = tmp * inc
-	end
-      end
+	  tmp = tmp + log(inc)
+      end end
       if tmp > most then most,out = tmp,h end
     end
-    return most
+    return out
   end
-    
+  -----------------------------------------------------    
   function nb(n)
-    n = n or nb0()
+    opt = opt or nb0()
     local log = abcd0()
     local t 
     for i,names,row in xys() do
-      if i > n.enough then
+      if i > opt.enough then
 	local actual    = row.y[1]
-	local predicted = predict(row,n,t,i,#t.subs,-1)
+	local predicted = predict(row,opt,t,i,#t.subs,-1)
 	abcd1(actual, predicted,log)
       end
       t = sample1(row,t,names)
