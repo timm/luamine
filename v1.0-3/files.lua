6a575e9900fb6ce82779db33fb004807967d51ea
Tim Menzies <tim@menzies.us>
2016-07-25 00:29:05 -0600

    saving

--- v1.0-3/files.lua
+++ v1.0-3/files.lua
@@ -1,21 +1,56 @@
-SEP      = "([^,]+)"       -- cell seperator
-DULL     = "['\"\t\n\r]*"  -- white space, quotes
-IGNORE   = "?"             -- columns, cells to ignore
-PADDING  = "%s*(.-)%s*"    -- space around words
-COMMENTS = "#.*"           -- comments
-WANTS    = function (t)
-    return  { -- first entry is defult, rest are special cases
-      {who= "_X_", what= sym0, wheres= {t.things, t.ins,  t.syms  }},
-      {who= "$",   what= num0, wheres= {t.things, t.ins,  t.nums  }},
-      {who= "<",   what= num0, wheres= {t.things, t.outs, t.nums, t.less}},
-      {who= ">",   what= num0, wheres= {t.things, t.outs, t.nums, t.more}},
-      {who= "=",   what= sym0, wheres= {t.things, t.outs, t.syms  }}}
+local IGNORE = "?"             -- columns, cells to ignore
+local nrows  = 0 -- counter for unique row ids (do not change)
+----------------------------------------------------------------
+local function map(t,f) for i,v in pairs(t) do f(v) end end
+local function same(x) return x end
+----------------------------------------------------------------
+local function thing1(i,one)
+  if one ~= IGNORE then
+    i.n = i.n + 1
+    i.add(i,one)
+    return i
+end end
+
+local function things(i,t)
+  if t then
+    map(t, function (one) thing1(i,one) end)
+  end
+  return i
 end
 
-local nrows=0 -- counter for unique row ids (do not change)
+local function sym1(i, one)
+  local old = i.counts[one]
+  local new = old and old + 1 or 1
+  i.counts[one] = new
+  if new > i.most then
+    i.most, i.mode = new,one
+  end
+end
 
+local function num1(i,one)
+  if one < i.lo then i.lo = one end
+  if one > i.up then i.up = one end
+  local delta = one - i.mu
+  i.mu = i.mu + delta / i.n
+  i.m2 = i.m2 + delta * (one - i.mu)
+end
+
+local function sd(i)     return i.n <= 1 and 0 or (i.m2 / (i.n - 1))^0.5 end
+local function norm(i,x) return (x - i.lo) / (i.up - i.lo + 1e-32)     end
+
+local function sym0(t)
+  return things({n=0, counts={}, most=0, mode=nil  ,add=sym1},t)
+end
+
+local function num0(t)
+  return things({n=0, mu=0, m2=0, up=-1e32, lo=1e32,add=num1},t)
+end
 ----------------------------------------------------------------
-function csv(f)
+local function csv(f)
+  local SEP      = "([^,]+)"       -- cell seperator
+  local DULL     = "['\"\t\n\r]*"  -- white space, quotes
+  local PADDING  = "%s*(.-)%s*"    -- space around words
+  local COMMENTS = "#.*"           -- comments
   if f then io.input(f) end  
   local first,line = true, io.read()
   local cache,use  = {},{}
@@ -44,27 +79,26 @@ function csv(f)
       line = io.read()
       if #row > 0 then return row end
 end end end
-
-function csv2tbl(f,     t)
-  for row in csv(f) do
-    t = row1(row, t)
-  end
-  return t
-end
-
-function row1(row, t)
-  local function whatWhere(cell,wanted)
-    for _,want in pairs(wanted) do
+----------------------------------------------------------------
+local function row1(row, t)
+  local function whatWhere(cell)
+    local WANTS =  { -- first entry is defult, rest are special cases
+      {who= "_X_", what= sym0, wheres= {t.things, t.ins,  t.syms  }},
+      {who= "$",   what= num0, wheres= {t.things, t.ins,  t.nums  }},
+      {who= "<",   what= num0, wheres= {t.things, t.outs, t.nums, t.less}},
+      {who= ">",   what= num0, wheres= {t.things, t.outs, t.nums, t.more}},
+      {who= "=",   what= sym0, wheres= {t.things, t.outs, t.syms  }}} 
+    local DEFAULT = WANTS[1]
+    for _,want in pairs(WANTS) do
       if string.find(cell,want.who) ~= nil then
 	return want.what, want.wheres
     end end
-    return wanted[1].what, wanted[1].wheres
+    return DEFAULT.what, DEFAULT.wheres
   end
   ------------------------------
   local function header(row,t)
-    wanted= WANTS(t)
     for col,cell in ipairs(row) do
-      what, wheres = whatWheres(cell, wanted)
+      local what, wheres = whatWheres(cell)
       local thing = what()
       thing.col = col
       for _,where in ipair(wheres) do
@@ -91,59 +125,25 @@ function row1(row, t)
   end
 end
 
-
-
-
-
-
-      
-function map(t,f) for i,v in pairs(t) do f(v) end end
-function same(x) return x end
-
-function sym0(t)
-  return things({n=0, counts={}, most=0, mode=nil  ,add=sym1},t)
-end
-
-function num0(t)
-  return things({n=0, mu=0, m2=0, up=-1e32, lo=1e32,add=num1},t)
-end
-
-function things(i,t)
-  map(t, function (one) thing1(i,one) end)
-  return i
-end
-
-function thing1(i,one)
-  if one ~= IGNORE then
-    i.n = i.n + 1
-    i.add(i,one)
-    return i
-end end
-  
-function sym1(i, one)
-  local old = t.counts[one]
-  local new = old and old + 1 or 1
-  t.counts[one] = new
-  if new > t.most then
-    t.most, t.mode = new,one
+local function csv2tbl(f,     t)
+  for row in csv(f) do
+    t = row1(row, t)
   end
+  return t
 end
 
-function num1(i,one)
-  if one < i.lo then i.lo = one end
-  if one > i.up then i.up = one end
-  local delta = one - i.mu
-  i.mu = i.mu + delta / i.n
-  i.m2 = i.m2 + delta * (one - i.mu)
-end
 
-function sd(i)     return i.n <= 1 and 0 or (i.m2 / (i.n - 1))^0.5 end
-function norm(i,x) return (x - i.lo) / (i.up - i.lo + 1e-32)     end
+-- n=0
+-- for line in cells() do
+--   n= n+1
+--   if line then
+--     print(n,#line, table.concat(line,","))
+--   end
+-- end
 
-n=0
-for line in cells() do
-  n= n+1
-  if line then
-    print(n,#line, table.concat(line,","))
-  end
-end
+return {map=map,same=same,
+	num0=num0, sym0=sym0,
+	thing1 = thing1,
+	sd=sd, norm=norm,
+	row1=row1,
+	csv2tbl=csv2tbl, csv=csv}
